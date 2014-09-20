ca87b8a9cfe06c94457a261fa2314b46e8d85445
Bronwyn Lewis <git@bronwynlewis.com>
2014-09-20 09:25:20 -0700

    updated documentation

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 JumPi
 =====
 
-Stuff for turning a Raspberry Pi into a network config jumphost
+Turning a Raspberry Pi into a remote access network config jumphost.
+
+The goal is to provide a small tool that can be plugged into equipment in a rack and then remotely accessed to complete upgrades, change configs, etc.
--- myIP.py
+++ myIP.py
@@ -3,15 +3,19 @@ import re
 import urllib2
 from sh import ifconfig
 
+# this uses sh, which allows you to interface with the subprocess ifconfig
 myIP = ifconfig("en0")
 
+# this just saves the raw ifconfig output for specified interface to txt
 file_ifconfig = open("jump1_ifconfig.txt", "w")
 file_ifconfig.writelines(myIP)
 file_ifconfig.close()
 
+# this opens the ifconfig output + allows you to interact with it as a string
 with open ("jump1_ifconfig.txt", "r") as myfile:
     data=myfile.read().replace('\n', '')
 
+# this checks what your public IP is
 def publicIP(myIP):
     ip_checker_url = "http://checkip.dyndns.org/"
     address_regexp = re.compile ('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
@@ -22,6 +26,7 @@ def publicIP(myIP):
     else:
             return None
 
+# this grabs your "private" IP from the raw ifconfig
 def privateIP(myIP):
     address_regexp = re.compile ('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
     response = data
@@ -31,6 +36,7 @@ def privateIP(myIP):
     else:
             return None
 
+# this writes your public and private IP to a text file
 file_ip = open("jump1_ip.txt", "w")
 file_ip.writelines("My public IP is: " + publicIP(myIP) + "\n")
 file_ip.writelines("My private IP is: " + privateIP(myIP))
