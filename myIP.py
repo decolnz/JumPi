4853b59606f105497fb8cdd0ca3faca77460bea4
Bronwyn Lewis <git@bronwynlewis.com>
2014-09-19 23:25:05 -0700

    exports public and private ip assignments to txt

--- myIP.py
+++ myIP.py
@@ -1,16 +1,37 @@
-# for using .write and .writelines
 import sys
-# for using ifconfig to pull interface data
+import re
+import urllib2
 from sh import ifconfig
 
-# creates/opens specified file
-file = open("/jump1_ip.txt", "w")
-
-# defines variable name for ifconfig output
 myIP = ifconfig("en0")
 
-# writes ifconfig output to specified file
-file.writelines(myIP)
+file_ifconfig = open("jump1_ifconfig.txt", "w")
+file_ifconfig.writelines(myIP)
+file_ifconfig.close()
 
-# closes file to free up processes
-file.close()
+with open ("jump1_ifconfig.txt", "r") as myfile:
+    data=myfile.read().replace('\n', '')
+
+def publicIP(myIP):
+    ip_checker_url = "http://checkip.dyndns.org/"
+    address_regexp = re.compile ('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+    response = urllib2.urlopen(ip_checker_url).read()
+    result = address_regexp.search(response)
+    if result:
+            return result.group()
+    else:
+            return None
+
+def privateIP(myIP):
+    address_regexp = re.compile ('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+    response = data
+    result = address_regexp.search(response)
+    if result:
+    	    return result.group()
+    else:
+            return None
+
+file_ip = open("jump1_ip.txt", "w")
+file_ip.writelines("My public IP is: " + publicIP(myIP) + "\n")
+file_ip.writelines("My private IP is: " + privateIP(myIP))
+file_ip.close()
