9766f1a004b606187c89ef764d65ff81159bf416
Bronwyn Lewis <git@bronwynlewis.com>
2014-10-02 00:06:54 -0700

    bash script for dhcp-hooks, clean-up of myIP.py, some btsync details

--- install.py
+++ install.py
@@ -1,15 +1,15 @@
 from subprocess import Popen, PIPE
 
 #installs PIP, which is needed to install sh for myIP.py
-Popen('apt-get install python-pip', shell=True, stdin=PIPE).communicate("y")
+Popen('apt-get install -y python-pip', shell=True, stdin=PIPE)
 
 #installs python-dev, which is needed for various commands to work
-Popen('apt-get install python-dev', shell=True, stdin=PIPE).communicate("y")
+Popen('apt-get install -y python-dev', shell=True, stdin=PIPE)
 
 #installs Screen so we can connect to console on routers/switches
-Popen('apt-get install screen', shell=True, stdin=PIPE).communicate("y")
+Popen('apt-get install -y screen', shell=True, stdin=PIPE)
 
 #installs openswan for L2TP VPN connectivity
-Popen('apt-get install openswan', shell=True, stdin=PIPE).communicate("y")
+Popen('apt-get install -y openswan', shell=True, stdin=PIPE)
 
-print "\n All done!"
+print "\nDon't forget to install sh via pip and configure btsync!"
