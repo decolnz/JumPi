aa179349bf84334dc309582d6b912c62373b925a
Bronwyn Lewis <git@bronwynlewis.com>
2014-10-02 10:14:52 -0700

    update install script so it just works for right now

--- install.py
+++ install.py
@@ -1,19 +1,19 @@
-from subprocess import Popen
+from subprocess import Popen, PIPE
 
 #installs PIP, which is needed to install sh for myIP.py
-Popen(['apt-get','install','-y','python-pip'], shell=True, stdin=None, stdout=None)
+Popen('apt-get install python-pip', shell=True, stdin=PIPE).communicate("y")
 
 #installs python-dev, which is needed for various commands to work
-Popen(['apt-get','install','-y','python-dev'], shell=True, stdin=None, stdout=None)
+Popen('apt-get install python-dev', shell=True, stdin=PIPE).communicate("y")
 
 #installs Screen so we can connect to console on routers/switches
-Popen(['apt-get','install','-y','screen'], shell=True, stdin=None, stdout=None)
+Popen('apt-get install screen', shell=True, stdin=PIPE).communicate("y")
 
 #installs MOSH
-Popen(['apt-get','install','-y','mosh'], shell=True, stdin=None, stdout=None)
+Popen('apt-get install mosh', shell=True, stdin=PIPE).communicate("y")
 
 #installs openswan for L2TP VPN connectivity
-#Popen('apt-get install -y openswan', shell=True, stdin=None, stdout=None)
+#Popen('apt-get install openswan', shell=True, stdin=PIPE).communicate("y")
 
 #installs sh via pip
 Popen('pip install sh', shell=True, stdin=None, stdout=None)
